7265070c7bcd2aeb1e1a2fa1e1e6b110a9252014
egoarka <egoarka@gmail.com>
2018-08-20 22:12:26 +0300

    feat(package): add jest's coverage

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "**/*.{ts,js}",
+    "!**/node_modules/**",
+    "!**/build/**",
+    "!**/coverage/**"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  coverageReporters: ["text", "text-summary"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
